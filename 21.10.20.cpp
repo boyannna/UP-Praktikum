fad74e6104e521d1fa0e577e244fcfaa2635b823
boyannna <72758830+boyannna@users.noreply.github.com>
2020-10-26 20:28:30 +0200

    Add files via upload

---
+++ 21.10.20.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{  /* 0 zadacha
+			double num1 = 0, num2 = 0;
+			char operation;
+			cin >>  num1 >> operation >> num2;
+			switch (operation) {
+			case '+':cout << (num1 + num2) << "\n"; break;
+			case '-':cout << (num1 - num2) << "\n"; break;
+			case '*':cout << (num1 * num2) << "\n"; break;
+			case '/':
+			if (num2 != 0)
+			{
+				cout << (num1 / num2) << "\n";
+			}
+			else
+			{
+				cout << "Divide by 0" << endl;
+			}
+			break;
+			default: cout << "Invalid" << endl; break;
+
+	1 zadacha
+			int year;
+			cin >> year;
+			if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0) {
+			cout << "wisokosna godina";
+			}
+			else {
+			cout << "ne e wisokosna";
+			}
+		2 zadacha
+			char symbol;
+				cin >> symbol;
+				if (symbol >= 'a' && symbol <= 'z') {
+					cout << (char)(symbol - 32);
+				}
+				else if (symbol >= 'A' && symbol <= 'Z') {
+					cout << (char)(symbol + 32);
+				}
+				else {
+					cout << "is not a letter";
+				}
+		3 zadacha
+			int num1 = 0;
+			cin >> num1;
+			switch (num1) {
+			case 1:cout << "zima" << "\n"; break;
+			case 2:cout << "zima" << "\n"; break;
+			case 12:cout << "zima" << "\n"; break;
+			case 3:cout << "prolet" << "\n"; break;
+			case 4:cout << "prolet" << "\n"; break;
+			case 5:cout << "prolet" << "\n"; break;
+			case 6:cout << "lqto" << "\n"; break;
+			case 7:cout << "lqto" << "\n"; break;
+			case 8:cout << "lqto" << "\n"; break;
+			case 9:cout << "esen" << "\n"; break;
+			case 10:cout << "esen" << "\n"; break;
+			case 11:cout << "esen" << "\n"; break;
+			}
+		4 zadacha
+		   int a, b;
+		   cin >> a >> b;
+		   if (a == 0 && b == 0) {
+		   cout << "INF";
+		}
+
+		   else if (a != 0 && b == 0) {
+		   cout << "INF";
+		}
+		   else {
+		   cout << b / a;
+		}
+	5 zadacha
+		int month, year;
+		cout << "month"<< endl ;
+		cin >> month;
+			cout << "year" << endl;
+			cin >> year;
+		switch (month) {
+		case 1:cout << "31" << "\n"; break;
+		case 3:cout << "31" << "\n"; break;
+		case 5:cout << "31" << "\n"; break;
+		case 7:cout << "31" << "\n"; break;
+		case 8:cout << "31" << "\n"; break;
+		case 10:cout << "31" << "\n"; break;
+		case 12:cout << "31" << "\n"; break;
+		case 4:cout << "30" << "\n"; break;
+		case 6:cout << "30" << "\n"; break;
+		case 9:cout << "30" << "\n"; break;
+		case 11:cout << "30" << "\n"; break;
+		}
+		if ((month==2)&&(year % 4 == 0 && year % 100 == 0 || year % 400 == 0)) {
+			cout << "28";
+		}
+		else {
+			cout << "29";
+		}
+	6 zadacha
+		int price, USB , GB_RAM, SSD;
+		cout << price << USB << GB_RAM << SSD
+		cin >> price >> USB >> GB_RAM >> SSD;
+		if (price >= 1000 && price <= 1500 && USB >= 3 && GB_RAM >= 8 && SSD) {
+
+		}
+		else (price <= 800 && (!SSD || GB_RAM < 8)) {
+		}
+*/
+		return 0;
+}
